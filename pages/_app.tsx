6cce48d4dd4073def3035ba24a104da7cb2773b3
crnorthc <crnorthc@eckerd.edu>
2021-10-13 17:40:55 -0400

    reconfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 import { Provider } from "react-redux"
-import store from "./store/store"
+import store from "../store/store"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
